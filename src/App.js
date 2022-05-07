111ed15309c43b611e2cdc671f2495cb27f9ed72
dodo2357 <dogukankalyoncuu@outlook.com>
2022-05-07 14:45:47 +0300

    second commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import ContactForm from './ContactForm';
+
+const App = () => {
+  return (
+    <div className='App'>
+      <div className='container'>
+        <h1 className='text-center'>TarGet Contact Us</h1>
+        <ContactForm />
+      </div>
+    </div>
+  );
+};
+
+export default App;
